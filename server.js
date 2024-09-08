4c8b84c5f1879c0c77cfdecf76ec70c70b3c6e57
chiragparmar2004 <chiragparmar4780@gmail.com>
2024-09-08 18:21:41 +0530

    td

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 const StartServer = async () => {
   const app = express();
 
-  const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 8080;
 
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true, limit: "1mb" }));
